dd3a1efe10707f36b85546f57b2df50fd9f48f96
marufrobin <marufrobin5@gmail.com>
2025-04-14 13:44:47 +0600

    🚀 init 🔥

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+console.log("Hello, world! from Robin");
+
+let name: string  ;
+
+name = "Robin";
+
+console.log(name)
+
+
+let names: (string| number|boolean) []= ["Robin", "John", "Doe",12,true];
+
+console.log(names)
+
+let random : string | number | boolean;
+
+random = "Robin";
+random = 123;
+random = true;
+console.log(random)
